caede21a56feb652839951489dbb5ad0d1f894db
Felipe Kcomt <felipe_kcomt@hotmail.com>
2023-03-06 20:32:32 -0500

    leave-room

--- src/index.ts
+++ src/index.ts
@@ -22,30 +22,36 @@ const playersInRoom: PlayerInRoomModel  = {};
 io.on("connection", function (socket) {
   console.log("Nuevo cliente conectado");
   socket.on('crear-room', (data) => {
-    console.log("data",data)
+    console.log("create-room", data)
       rooms.push({ roomName: `room-${data.roomName}`, players: [socket.id], namePlayer:[data.name] });
-      socket.join(data.id);
-      if (!playersInRoom[data.id])
+      socket.join(data.idRoom);
+      if (!playersInRoom[data.idRoom])
       {
-        playersInRoom[data.id] = [];
+        playersInRoom[data.idRoom] = [];
       }
-      playersInRoom[data.id].push({ id: socket.id, name: data.name, user:data.user })
-      console.log("playersInRoom[data.id]", playersInRoom[data.id])
-      io.to(data.id).emit("playersInRoom", playersInRoom[data.id] )
-      console.log(`Se ha creado el room con id: ${data.id} y el usuario : ${data.name}`);
+      playersInRoom[data.idRoom].push({ id: socket.id, name: data.name, user:data.user, idUser: data.user.id })
+      io.to(data.idRoom).emit("playersInRoom", playersInRoom[data.idRoom] )
+      console.log(`Se ha creado el room con id: ${data.idRoom} y el usuario : ${data.name}`);
   });
   socket.on('join-room', (data) => {
     if (data) {
-    socket.join(data.id);
-    playersInRoom[data.id].push({ id: socket.id, name: data.name, user: data.user })
-    console.log("playersInRoom[data.id]", playersInRoom[data.id])
-    io.to(data.id).emit("playersInRoom", playersInRoom[data.id] )
-    console.log(`Se ha unido al room con id: ${data.id} el usuario : ${data.name}`);
+    socket.join(data.idRoom);
+    playersInRoom[data.idRoom].push({ id: socket.id, name: data.user.name, user: data.user, idUser: data.user.id })
+    io.to(data.idRoom).emit("playersInRoom", playersInRoom[data.idRoom] )
+    console.log(`Se ha unido al room con id: ${data.idRoom} el usuario : ${data.name}`);
   } else {
     console.log("Error al emitir el evento")
   }
-
   });
+  socket.on('leave-room', (data) => {
+    console.log("data",data)
+    console.log("playersInRoom[data.idRoom]",playersInRoom)
+    console.log("playersInRoom[data.idRoom]",playersInRoom[data.idRoom])
+    const playeinRoomLeavePlayer =  playersInRoom[data.idRoom].filter((item: any) => item.idUser !== data.idUser)
+    console.log("playeinRoomLeavePlayer",playeinRoomLeavePlayer)
+    playersInRoom[data.idRoom] = playeinRoomLeavePlayer
+    io.to(data.idRoom).emit("playersInRoom", playersInRoom[data.idRoom] )
+  })
   socket.on("disconnect", () => {
     console.log(`usuario desconectado`)
   });
