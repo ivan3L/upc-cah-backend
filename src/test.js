2e857e0b2dad9a66577d039e2b1392caeb2b2117
Felipe Kcomt <felipe_kcomt@hotmail.com>
2023-06-22 13:01:39 -0500

    test

--- src/test.js
+++ src/test.js
@@ -1,11 +1,5 @@
 import http from "k6/http";
 
-export let options = {
-  vus: 3,
-  iterations: 5,
-  // duration: '12s',
-};
-
 export default function () {
   const res = http.get("http://localhost:8080/test");
   //const res = http.get('http://loadbalancer.wtm-upc.online:8080/test');
